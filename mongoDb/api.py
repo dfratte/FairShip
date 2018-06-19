bd8169af62f4c60fca2092f759a783ff7c8d35a4
Pranav <bhatnagar1989@hotmail.com>
2018-06-19 16:42:06 +0200

    added IOV search for subdetector

--- mongoDb/api.py
+++ mongoDb/api.py
@@ -14,13 +14,23 @@ def list_subdetectors():
 
 
 def show_subdetector(searched_name):
-    return Subdetector.objects(name=searched_name)
+    return Subdetector.objects(name=searched_name).first()
 
 
 def show_subdetector_conditions(searched_name):
     return show_subdetector(searched_name).values_list('conditions')
+
     # return Subdetector.objects(name=searched_name).values_list('conditions')
 
 
 def show_subdetector_condition(searched_name, searched_condition):
-    return Subdetector.objects(name=searched_name).first().conditions.filter(name=searched_condition).first()
+    # return Subdetector.objects(name=searched_name).first().conditions.filter(name=searched_condition).first()
+    return Subdetector.objects(name=searched_name).first().conditions.filter(iov=searched_condition).first()
+
+
+def show_subdetector_iovs(searched_name, searched_iovs):
+    return Subdetector.objects(name=searched_name).first().conditions.filter(iov=searched_iovs).first()
+
+
+def show_subdetector_iov(searched_name, searched_iov):
+    return Subdetector.objects(name=searched_name).first().conditions.filter(iov=searched_iov).first()
