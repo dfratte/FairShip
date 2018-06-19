c19130e46f6fe349fc48b3548cf9cca876a79bc2
Daniel <danielrfratte@gmail.com>
2018-06-19 14:26:28 +0200

    Add support for show subdetector and condition flag

--- mongoDb/cdbService.py
+++ mongoDb/cdbService.py
@@ -24,11 +24,6 @@ This script is used to retrieve condition data from a condition database.
 parser = argparse.ArgumentParser(description=help_desc,
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
 
-parser.add_argument('-lg', '--list-global-tags',
-                    dest='list_global_tags',
-                    help='Flag to retrieve all global tags of the conditions db',
-                    action="store_true")
-
 parser.add_argument('-ls', '--list-subdetectors',
                     dest='list_subdetectors',
                     help='Flag to retrieve all subdetectors of the conditions db',
@@ -40,10 +35,27 @@ parser.add_argument('-sd', '--subdetector',
                     required=False,
                     help='Name of the subdetector to retrieve.')
 
+parser.add_argument('-c', '--condition',
+                    dest='condition',
+                    default=None,
+                    required=False,
+                    help='Name of the condition to retrieve.')
+
 args = parser.parse_args()
+
+# flag to check that a subdetector exists when querying for a condition
+# TODO: move this to an array of flags/error msg and analyze it in a separate function
+flag_sd_c = vars(args)['subdetector'] is None and vars(args)['condition'] is not None
+
+if flag_sd_c:
+    parser.error('arguments error')
+else:
+    # call show condition function
+    print "To be implemented!"
+    
 
 if args.list_subdetectors:
     list_subdetectors()
 
-if args.subdetector is not None:
+elif args.subdetector is not None and args.condition is None:
     show_subdetector(args.subdetector)
