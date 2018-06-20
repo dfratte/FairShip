a907019709bb1e1f095a64ac0583dc7748c84bc2
Giovanni Calheiros <g.de.almeida.calheiros@tue.nl>
2018-06-20 16:36:16 +0200

    Add new tests to the test_api

--- mongoDb/test_api.py
+++ mongoDb/test_api.py
@@ -5,13 +5,15 @@ Unittest developed to guarantee the api integrity
 import unittest
 
 from api import API
-from models import Subdetector
+from models import Condition, Subdetector
 
 API = API()
 
 SUBDETECTOR_1 = Subdetector(name='subdetector_test_1')
 SUBDETECTOR_2 = Subdetector(name='subdetector_test_2')
-SUBDETECTOR_3 = Subdetector(name='subdetector_test_3')
+
+CONDITION_1 = Condition(name='condition_test_1')
+CONDITION_2 = Condition(name='condition_test_2')
 
 
 class TestApi(unittest.TestCase):
@@ -20,15 +22,15 @@ class TestApi(unittest.TestCase):
     def setUpClass(cls):
         super(TestApi, cls).setUpClass()
         # TODO use the correct method to add the information on database instead of add manually....
+        SUBDETECTOR_1.conditions.append(CONDITION_1)
+        SUBDETECTOR_1.conditions.append(CONDITION_2)
         SUBDETECTOR_1.save()
         SUBDETECTOR_2.save()
-        SUBDETECTOR_3.save()
 
     @classmethod
     def tearDownClass(cls):
         SUBDETECTOR_1.delete()
         SUBDETECTOR_2.delete()
-        SUBDETECTOR_3.delete()
 
     def test_list_subdetectors(self):
         """Method that evaluate if the list subdetector method is returning a collection of subdetectors.
@@ -36,13 +38,28 @@ class TestApi(unittest.TestCase):
         This test do not verify the conten of the returned list
         """
         subdetectors = API.list_subdetectors()
-        self.assertGreaterEqual(subdetectors.count(), 3)
+        self.assertGreaterEqual(subdetectors.count(), 2)
 
     def test_show_subdetector(self):
         subdetector = API.show_subdetector(SUBDETECTOR_1.name)
-        self.assertIsNotNone(subdetector)
         self.assertEqual(subdetector.name, SUBDETECTOR_1.name)
 
     def test_show_subdetector_not_found(self):
         subdetector = API.show_subdetector('invalid_subdetector')
         self.assertIsNone(subdetector)
+
+    def test_show_subdetector_conditions(self):
+        conditions = API.show_subdetector_conditions(SUBDETECTOR_1.name)
+        self.assertEqual(conditions.count(), 2)
+
+    def test_show_subdetector_whitout_conditions(self):
+        conditions = API.show_subdetector_conditions(SUBDETECTOR_2.name)
+        self.assertEqual(conditions.count(), 0)
+
+    def test_show_subdetector_condition(self):
+        condition = API.show_subdetector_condition(SUBDETECTOR_1.name, CONDITION_1.name)
+        self.assertEqual(condition.name, CONDITION_1.name)
+
+    def test_show_subdetector_with_invalid_condition(self):
+        condition = API.show_subdetector_condition(SUBDETECTOR_1.name, 'invalid_condition')
+        self.assertIsNone(condition)
