35b5eb51a102ac44eb437ba72832ce8fb1b1ce80
Kostas <konskarm@gmail.com>
2018-06-21 14:11:49 +0200

    Added doxygen documentation comments for the models.

--- mongoDb/models.py
+++ mongoDb/models.py
@@ -4,17 +4,51 @@ MongoDb Models
 from mongoengine import Document, EmbeddedDocument, EmbeddedDocumentListField, StringField, DateTimeField, \
     ReferenceField
 
+
+# TODO Review the class definitions (Parameter, Source, Condition, Subdetector)
+
 class Source(Document):
+    """
+    Source
+    """
+    ##
+    # @var name
+    # Name of the Source
     name = StringField(max_length=1000, null=True)
 
 
 class Parameter(EmbeddedDocument):
+    """
+    Parameter refers to the specific condition of a subdetector e.g. Temperature
+    """
+    ##
+    # @var name
+    # Name of the Parameter
+    # @var iov
+    # Interval of Validity
+    # @var value
+    # Value of the Parameter
     name = StringField(max_length=1000, null=True)
     iov = DateTimeField(null=True)
     value = StringField(max_length=1000, null=True)
 
 
 class Condition(EmbeddedDocument):
+    """
+    It refers to the setup and configuration of specific instrumentation utilized for carrying out certain
+    HEP experiment.
+    """
+    ##
+    # @var name
+    # Name of the Condition
+    # @var iov
+    # Interval of Validity
+    # @var tag
+    # Tag of the Condition
+    # @var parameters
+    # Collection of parameters related to that Condition
+    # @var source
+    # Source used to group Conditions
     name = StringField(max_length=1000, null=True)
     iov = DateTimeField(null=True)
     tag = StringField(max_length=1000, null=True)
@@ -23,5 +57,13 @@ class Condition(EmbeddedDocument):
 
 
 class Subdetector(Document):
+    """
+    Components in which a Detector can be broken down to.
+    """
+    ##
+    # @var name
+    # Name of the Subdetector
+    # @var conditions
+    # Conditions evaluated by the Subdetector
     name = StringField(max_length=1000, null=True)
     conditions = EmbeddedDocumentListField(Condition)
